796ae0e95f8c8ed1475cfe79331993b4fa3908da
Karthik Arun <karthikarun@in.ibm.com>
2020-08-14 16:16:27 +0530

    OSCAR - Registration. Look up classes filter, chrome extension

--- filter.js
+++ filter.js
@@ -1,6 +1,7 @@
-// Script credits: ningun0@tutanota.com
-// Modified by: karun3@gatech.edu
+// Script credits: Fellow student (identity removed for privacy)
+// Modified by: Karthik Arun
 // Get only OMSCS courses.
+
 $( "td" ).removeClass( "dddefault" );
 $(".datadisplaytable tr td:contains('OCY')").parent().remove();
 $(".datadisplaytable tr td:contains('OAN')").parent().remove();
